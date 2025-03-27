d67ab9d89c63754186f8f945a3fef349cec6302d
Phạm Quang Vũ <133316521+FirstOne2308@users.noreply.github.com>
2025-03-27 16:41:46 +0700

    Update Main.java

--- BTTH1/src/Main.java
+++ BTTH1/src/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //Bai 1
-//        float r = sc.nextFloat();
-//        Bai_1 bai1 = new Bai_1();
-//        bai1.DienTichHinhTron(r);
+        float r = sc.nextFloat();
+        Bai_1 bai1 = new Bai_1();
+        bai1.DienTichHinhTron(r);
 //        //Bai 2
-//        Bai_2 bai2 = new Bai_2();
-//        bai2.TinhSoPi();
+        Bai_2 bai2 = new Bai_2();
+        bai2.TinhSoPi();
         //Bai 3
         Bai_3 bai3 = new Bai_3();
         bai3.Output();
